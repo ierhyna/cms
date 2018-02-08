5bc671f8bdc0293616f6b03594aec1cedcede8c8
Irina Sokolovskaja <sokolovskaja.irina@gmail.com>
2018-02-08 22:06:39 +0300

    Init

---
+++ index.js
@@ -0,0 +1,40 @@
+const ejs = require('ejs');
+const fs = require('fs');
+
+const template = 'themes/one/index.ejs';
+const source = 'content/';
+const dest = '_site/';
+
+fs.readFile(template, {
+  encoding: 'utf-8'
+}, (error, tpl) => {
+  if (error) {
+    console.log(`Template ${source}/${file} cannot be read`);
+  } else {
+
+    fs.readdir(source, (err, files) => {
+      files.forEach(file => {
+        fs.readFile(`${source}/${file}`, {
+          encoding: 'utf-8'
+        }, (error, data) => {
+
+          if (error) {
+            console.log(`File ${source}/${file} cannot be read`);
+          } else {
+            const html = ejs.render(tpl, { content: data });
+
+            fs.writeFile(`${dest}/${file}` + ".html", html, error => {
+              if (error) {
+                console.log("could not save file")
+              } else {
+                console.log("file saved as " + file + ".html")
+              }
+            })
+          }
+
+        });
+      })
+    });
+
+  }
+});
