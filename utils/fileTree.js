c537d185b884d9eb177be4ad6a8ae37552ee1edf
Alex Bykov <spbeat@mail.ru>
2018-02-10 19:51:00 +0300

    filetree

--- utils/fileTree.js
+++ utils/fileTree.js
@@ -1,10 +1,12 @@
-const unslash = require("unslash");
+const unslash = require("./unslash");
+const fs = require("fs");
+const path = require("path");
 
-module.exports = function(dir) {
+module.exports = function fileTree(dir) {
     return fs.statSync(dir).isDirectory() ?
         Array.prototype
         .concat(...fs.readdirSync(dir)
-            .map(f => buildTree(path.join(dir, f))))
+            .map(f => fileTree(path.join(dir, f))))
         .map(f => unslash(f)) :
         dir;
 }
