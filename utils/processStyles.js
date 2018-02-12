b338cf9de1d5ea472f740a12cab62b1a4bac7922
Alex B <spbeat@mail.ru>
2018-02-12 15:29:29 +0300

    add processStyles as util

---
+++ utils/processStyles.js
@@ -0,0 +1,18 @@
+const fileTree = require("./fileTree");
+const err = require("./err");
+const fs = require("fs");
+const path = require("path");
+
+module.exports = function(theme, options) {
+  fileTree(theme, "css").forEach(style => {
+    try {
+      fs.copyFileSync(
+        `${options.theme}/${path.parse(style).base}`,
+        `${options.dest}/${path.parse(style).base}`
+      );
+    } catch (error) {
+      err(`Could not copy ${path.parse(style).base}`);
+      err(error);
+    }
+  });
+};
