ea6092b56da9986857ade87a9b3c27a53497d743
Irina Sokolovskaja <sokolovskaja.irina@gmail.com>
2018-02-12 23:45:27 +0300

    Cleanup before generating new build

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const yaml = require("js-yaml");
 const makeDir = require("make-dir");
+const del = require("del");
 
 const fileTree = require("./utils/fileTree");
 const processImages = require("./utils/processImages");
@@ -16,9 +17,10 @@ const options = {
   template: fs.readFileSync(`${theme}/index.ejs`).toString("utf-8")
 };
 
+del.sync([options.dest]);
 makeDir.sync(options.dest + options.imageTarget);
-const markdownFiles = fileTree(options.source, "md");
 
+const markdownFiles = fileTree(options.source, "md");
 const blogPostCache = processMarkdown(markdownFiles, options);
 
 processStyles(theme, options);
