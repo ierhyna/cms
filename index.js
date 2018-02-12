f46bdad20b6462ff9b26240ba46ca0aae7119c32
Alex B <spbeat@mail.ru>
2018-02-12 15:11:36 +0300

    moves relevent settings to yaml

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
 const fs = require("fs");
 const yaml = require("js-yaml");
 const makeDir = require("make-dir");
+const path = require("path");
+
 const fileTree = require("./utils/fileTree");
+const err = require("./utils/err");
+const log = require("./utils/log");
+const processImages = require("./utils/processImages");
 const buildStatic = require("./utils/buildStatic");
 const buildIndex = require("./utils/buildIndex");
-const log = require("./utils/log");
-const err = require("./utils/err");
-const processImages = require("./utils/processImages");
 
 const config = yaml.safeLoad(fs.readFileSync("config.yaml", "utf8"));
-const theme = `themes/${config.theme}`;
+
+const theme = `themes/${config.theme}/`;
 const options = {
+  ...config.paths,
   theme,
-  dest: "_site/",
-  template: fs.readFileSync(`${theme}/index.ejs`).toString("utf-8"),
-  source: "content/",
-  static: "static/",
-  imageDirectory: "content/images/",
-  imageTarget: "static/images/",
-  styles: ["style.css"]
+  template: fs.readFileSync(`${theme}/index.ejs`).toString("utf-8")
 };
+
 const markdownFiles = fileTree(options.source, "md");
-//const imageFiles = fileTree(options.imageDirectory);
 const blogPostCache = [];
 
 // Process styles
 makeDir.sync(options.imageTarget);
-options.styles.forEach(style =>
+fileTree(theme, "css").forEach(style =>
   fs.copyFile(
-    `${options.theme}/${style}`,
-    `${options.dest}/${style}`,
+    `${options.theme}/${path.parse(style).base}`,
+    `${options.dest}/${path.parse(style).base}`,
     error => {
       if (error) {
         err(error, `Could not copy ${style}`);
