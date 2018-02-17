57ceff8239c7a02cf3a93e1861422d329283ee76
Irina Sokolovskaja <sokolovskaja.irina@gmail.com>
2018-02-17 18:46:25 +0300

    Process images

--- utils/processImages.js
+++ utils/processImages.js
@@ -1,10 +1,18 @@
 const imagemin = require("imagemin");
+const imageminJpeg = require("imagemin-jpeg-recompress");
+const imageminPng = require("imagemin-pngquant");
 const { log, err } = require("./log");
 
 module.exports = function(options) {
   imagemin(
     [`${options.imageDirectory}*.{jpg,png}`],
-    `${options.dest}${options.imageTarget}`
+    `${options.dest}${options.imageTarget}`,
+    {
+      plugins: [
+        imageminJpeg({ quality: "medium" }),
+        imageminPng({ quality: "65-80" })
+      ]
+    }
   )
     .then(() => log("images copied to static"))
     .catch(e => err("image processing failed", e));
